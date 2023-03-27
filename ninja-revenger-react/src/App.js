bb9ce34d0bbf297bc4b642310f118fdb7380ae2f
Millyaa-11 <chalitaes@gmail.com>
2023-03-28 00:56:39 +0700

    upload

---
+++ ninja-revenger-react/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import * as React from 'react';
+
+
+function App() {
+
+  return (
+    <div className='Ninja-Revengers'>
+        <div className='menu container'>
+            <div className='bg-1'><bg-1 style={{ minHeight: '100%' }} />
+            <div className='lightning-small'>
+              <img src={require('./pic/light_small.png')} alt="lightning" />
+            </div>
+            </div>
+        </div>
+        <button class="start-bt1">start</button>
+    </div>
+  );
+}
+
+export default App;
