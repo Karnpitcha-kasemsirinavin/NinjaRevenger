e47b27d91e2b060746c3d36497cbb7f56317c4f5
Millyaa-11 <chalitaes@gmail.com>
2023-03-30 01:55:23 +0700

    css

--- ninja-revenger-react/src/App.js
+++ ninja-revenger-react/src/App.js
@@ -2,19 +2,13 @@ import './App.css';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { useRef } from 'react';  
-import { BigScreen } from "./components/big-screen/components.jsx";
+import { BigScreen } from "./home/big-screen/components.jsx";
+import { Desktop } from "./home/desktop/components.jsx";
 
 
 function App() {
   const windowSize = useRef([window.innerWidth, window.innerHeight])
 
-  const isLaptop = useMediaQuery({
-    query: "(min-device-width: 769px)",
-  });
-
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 1025px)",
-  });
 
   const isBigScreen = useMediaQuery({
     query: "(min-device-width: 1201px )",
@@ -22,10 +16,7 @@ function App() {
 
   return (
   <div>
-    {/* <h2>Width: {windowSize.current[0]}</h2> */}   
-
-    {isBigScreen && <BigScreen />}
-
+    <BigScreen />
   </div>
   );
 }
