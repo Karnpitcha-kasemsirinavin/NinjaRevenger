e522c8beb8370e649080160ee9b86c3b2f680ba3
Chakrit Ritthakananon <agentpenguin202@gmail.com>
2023-03-29 22:19:11 +0700

    changes

--- ninja-revenger-react/src/components/big-screen/components.jsx
+++ ninja-revenger-react/src/components/big-screen/components.jsx
@@ -7,11 +7,11 @@ export const BigScreen = () => {
             <div className='Ninja-Revengers'>
                 <div className='container'>
                     <div className='bg-1'>
-                    <img className='lightning-big' src={require("./pic/light_big.png")} alt="lighning-b" />
-                    <img className='lightning-small center'src={require('./pic/light_small.png')} alt="lightning" />
-                    <img className='ninja-menu' src={require("./pic/ninja-1.png")} alt="ninja1" />
-                    <img className='smoke-grd' src={require("./pic/smoke_ground.png")} alt="smoke-grd" />
-                    <img className='game-title' src={require("./pic/ninja-title.png")} alt="title" />
+                    <img className='lightning-big' src={require("../../images/light_big.png")} alt="lighning-b" />
+                    <img className='lightning-small center'src={require('../../images/light_small.png')} alt="lightning" />
+                    <img className='ninja-menu' src={require("../../images/ninja-1.png")} alt="ninja1" />
+                    <img className='smoke-grd' src={require("../../images/smoke_ground.png")} alt="smoke-grd" />
+                    <img className='game-title' src={require("../../images/ninja-title.png")} alt="title" />
                     <button class="start-bt1">start</button>
                     <button class="exit-bt1">start</button>
                     </div>
