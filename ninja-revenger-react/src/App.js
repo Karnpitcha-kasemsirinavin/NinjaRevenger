aa8ad192029e72bd3c0349f8ea6652548de6ec1a
Millyaa-11 <chalitaes@gmail.com>
2023-03-30 03:54:36 +0700

    responsive fin

--- ninja-revenger-react/src/App.js
+++ ninja-revenger-react/src/App.js
@@ -3,20 +3,16 @@ import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { useRef } from 'react';  
 import { BigScreen } from "./home/big-screen/components.jsx";
-import { Desktop } from "./home/desktop/components.jsx";
+
 
 
 function App() {
   const windowSize = useRef([window.innerWidth, window.innerHeight])
 
-
-  const isBigScreen = useMediaQuery({
-    query: "(min-device-width: 1201px )",
-  });
-
   return (
   <div>
-    <BigScreen />
+    {/* {<h2>Width: {windowSize.current[0]}</h2>}    */}
+      <BigScreen/>
   </div>
   );
 }
