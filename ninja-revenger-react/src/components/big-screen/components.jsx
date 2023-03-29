ec662ece92a56087dfd75346167f6239f74f55a4
Millyaa-11 <chalitaes@gmail.com>
2023-03-29 19:28:15 +0700

    update

--- ninja-revenger-react/src/components/big-screen/components.jsx
+++ ninja-revenger-react/src/components/big-screen/components.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import './big-screen.style.css'
+import './style.css'
+
 export const BigScreen = () => {
     return (
         <div className="big-screen">
-            <p>Whoops! I'm in big screen mode.</p>
-            <p>This is the base of the pyramid</p>
+            <div className='Ninja-Revengers'>
+                <div className='menu container'>
+                    <div className='bg-1'>
+                    <img className='lightning-big' src={require("./pic/smoke_ground.png")} alt="lighning-b" />
+                    <img className='lightning-small center'src={require('./pic/light_small.png')} alt="lightning" />
+                    <img className='ninja-menu' src={require("./pic/ninja-1.png")} alt="ninja1" />
+                    <img className='smoke-grd' src={require("./pic/smoke_ground.png")} alt="smoke-grd" />
+                    <img className='game-title' src={require("./pic/ninja-title.png")} alt="title" />
+                    <button class="start-bt1">start</button>
+                    <button class="exit-bt1">start</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
