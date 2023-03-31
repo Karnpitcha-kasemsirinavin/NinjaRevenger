8f75af859c74802c39d2faa0e182050ea942d49b
Millyaa-11 <chalitaes@gmail.com>
2023-04-01 01:52:03 +0700

    doing player profile and detail

--- ninja-revenger-react/src/pages/main-game/main-game-component.jsx
+++ ninja-revenger-react/src/pages/main-game/main-game-component.jsx
@@ -3,18 +3,26 @@ import './style.css'
 
 export const MainGame = () => {
 	return (
-        <div className='container'>
-            <div className='cam left'>
-                <img
-                    className='profile left'
-                    src={require("../../images/user-profile-example.png")}
-                    alt="profile-left"
-				/>
+    <div className='container'>
+        <div className='cam-left'>
+            <img
+                className='profile-left'
+                src={require("../../images/user-profile-example.png")}
+                alt="profile-left"
+            />
+            <div className='player-detail'>
+                <p className='username-left'>Natasha Romanoff</p>
             </div>
-            <div className='cam right'>
-                
-            </div>
+        </div>
+        <div className='cam-right'>
+            <img
+                className='profile-right'
+                src={require("../..//images/user-profile2-example.jpg")}
+                alt="profile-right"
+            />
             
         </div>
+    </div>
+
     )
     }
