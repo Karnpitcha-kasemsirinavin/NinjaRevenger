281b4d5b69235c591f13cf53b3f04fdb62dc30fa
Chakrit Ritthakananon <agentpenguin202@gmail.com>
2023-04-01 02:16:40 +0700

    updated

--- ninja-revenger-react/src/pages/Home/components.jsx
+++ ninja-revenger-react/src/pages/Home/components.jsx
@@ -4,38 +4,40 @@ import bg1 from "../../images/background-nolight.png"
 
 export const BigScreen = () => {
 	return (
-		<div className="big-screen">
-			<div className='container'>
-				<div className='bg-1' src={bg1}>
-					<img
-						className='lightning-big'
-						src={require("../../images/light_big.png")}
-						alt="lighning-b"
-					/>
-					<img
-						className='lightning-small center'
-						src={require('../../images/light_small.png')}
-						alt="lightning"
-					/>
-					<img
-						className='ninja-menu'
-						src={require("../../images/ninja-1.png")}
-						alt="ninja1"
-					/>
-					<img
-						className='smoke-grd'
-						src={require("../../images/smoke_ground.png")}
-						alt="smoke-grd"
-					/>
-					<img
-						className='game-title'
-						src={require("../../images/ninja-title.png")}
-						alt="title"
-					/>
-					<button class="start-bt1">start</button>
-					<button class="exit-bt1">start</button>
-				</div>
-			</div>
+		<div className='container'>
+			<img 
+				className='bg-1'
+				src={bg1}
+				alt="bg-1"
+			/>
+			<img
+				className='lightning-big'
+				src={require("../../images/Group 3.png")}
+				alt="lighning-b"
+			/>
+			<img
+				className='lightning-small center'
+				src={require('../../images/light_small.png')}
+				alt="lightning"
+			/>
+			<img
+				className='ninja-menu'
+				src={require("../../images/ninja-1.png")}
+				alt="ninja1"
+			/>
+			<img
+				className='smoke-grd'
+				src={require("../../images/smoke_ground.png")}
+				alt="smoke-grd"
+			/>
+			<img
+				className='game-title'
+				src={require("../../images/ninja-title.png")}
+				alt="title"
+			/>
+			<button class="start-bt1">Play with Friend</button>
+			<button class="start-bt2">Play with Stranger</button>
+			<button class="exit-bt1">Exit</button>
 		</div>
 	)
 }
