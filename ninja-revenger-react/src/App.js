5c68c6cab8e1107b57413ad0686ab107d6154713
Millyaa-11 <chalitaes@gmail.com>
2023-04-01 21:01:10 +0700

    profile

--- ninja-revenger-react/src/App.js
+++ ninja-revenger-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { useRef } from 'react';  
 import { MainGame } from'./pages/main-game/main-game-component.jsx'
+import { BigScreen } from './pages/Home/components.jsx';
 
 
 
--- ninja-revenger-react/src/pages/main-game/main-game-component.jsx
+++ ninja-revenger-react/src/pages/main-game/main-game-component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 
 export const MainGame = () => {
-	return (
+    return (
     <div className='container'>
         <div className='cam-left'>
             <div className='wrapper'>
@@ -11,16 +11,26 @@ export const MainGame = () => {
                     src={require("../../images/user-profile-example.png")}
                     alt="profile-left"
                 />
-                <p className='player-detail'>Natasha Romanoff</p>
+                <p className='player-detail-left'>Natasha Romanoff</p>
+                <img className='stars-l'
+                    src={require("../../images/star0.png")}
+                    alt='star0'
+                    />
             </div>
         </div>
         <div className='cam-right'>
-            <img
-                className='profile-right'
-                src={require("../..//images/user-profile2-example.jpg")}
-                alt="profile-right"
-            />
-            
+            <div className='wrapper'>
+                <p className='player-detail-right'>Natasha Romanoff</p>
+                <img className='stars'
+                    src={require("../../images/star0.png")}
+                    alt='star0'
+                />
+                <img
+                    className='profile-right'
+                    src={require("../..//images/user-profile2-example.jpg")}
+                    alt="profile-right"
+                />
+            </div>
         </div>
     </div>
 
