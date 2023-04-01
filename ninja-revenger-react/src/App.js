0c6fe04c1cd16a7e957456dc7be7c42499db37de
Chakrit Ritthakananon <agentpenguin202@gmail.com>
2023-04-01 22:51:59 +0700

    finished

--- ninja-revenger-react/src/App.js
+++ ninja-revenger-react/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import React from 'react';
-import { useRef } from 'react';  
+// import { useRef } from 'react';  
 import { MainGame } from'./pages/main-game/main-game-component.jsx'
 import { BigScreen } from './pages/Home/components.jsx';
 
 
 
 function App() {
-  const windowSize = useRef([window.innerWidth, window.innerHeight])
+  // const windowSize = useRef([window.innerWidth, window.innerHeight])
 
   return (
   <div>
     {/* {<h2>Width: {windowSize.current[0]}</h2>}    */}
-    {/* <BigScreen/> */}
-    <MainGame/>
+    <BigScreen/>
+    {/* <MainGame/> */}
   </div>
   );
 }
