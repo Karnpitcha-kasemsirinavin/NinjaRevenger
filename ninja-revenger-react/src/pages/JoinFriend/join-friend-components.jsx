77bc33dca1db67ea49310f0c363031ad383672ff
Millyaa-11 <chalitaes@gmail.com>
2023-04-07 02:19:19 +0700

    join friend finish

--- ninja-revenger-react/src/pages/JoinFriend/join-friend-components.jsx
+++ ninja-revenger-react/src/pages/JoinFriend/join-friend-components.jsx
@@ -5,13 +5,14 @@ export const JoinFriend = () => {
     return (
     <div class="background-container">
         <div class="background-animation">
+        <img
+            className='join-game-title'
+            src={require("../../images/jointitle.png")}
+            alt='join-title'
+        />
         <form action="/url" method="GET">
             <input type="text" class="link-box" id="link" name='link'/>
             <button class="bt-link" role="link-button">Link</button>
-        </form>
-        <form action="/url" method="GET">
-            <input type="text" class="friend-box" id="friend" name='friend'/>
-            <button class="bt-friend" role="friend-button">Friend</button>
         </form>
         </div>
     </div> 
