a3a5127617ea6e291600f760caaad24a05008fa3
Millyaa-11 <chalitaes@gmail.com>
2023-04-29 20:50:34 +0700

    commented

--- cognitive-project-mediapipe/hand-detection.js
+++ cognitive-project-mediapipe/hand-detection.js
@@ -1,12 +1,17 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
+
+//function for hands appearing on cam
 function onResults(results) {
+  //setting the canvas
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  //if there is hand detected
   if (results.multiHandLandmarks) {
+    //detect how many there are label hand 1 and 2
     for (let i = 0; i < results.multiHandLandmarks.length; i++) {
       const landmarks = results.multiHandLandmarks[i];
       if (results.multiHandedness) {
@@ -18,19 +23,21 @@ function onResults(results) {
         }
         console.log(`${hand}:`);
       }
+      
+      //print the landmarks position
       if (landmarks.length > 0) {
-        for (let j = 0; j < landmarks.length; j++) {
-          const landmark = landmarks[j];
+        for (let j = 0; j < landmarks.length; j++) {const landmark = landmarks[j];
           console.log(`Landmark ${j}: (${landmark.x}, ${landmark.y}, ${landmark.z})`);
         }
-        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                       {color: '#00FF00', lineWidth: 5});
+        //draw the connectors of landmarks and draw landmarks
+        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,{color: '#00FF00', lineWidth: 5});
         drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
       }
     }
   }
   canvasCtx.restore();
 }
+// using the hand object from mediapipe
 const hands = new Hands({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.3.1632795355/${file}`;
 }});
@@ -40,8 +47,10 @@ hands.setOptions({
   minTrackingConfidence: 0.5
 });
 
+//execute onResults function if hand is detected
 hands.onResults(onResults);
 
+//set the camera settings and send images
 const camera = new Camera(videoElement, {
   onFrame: async () => {
     await hands.send({image: videoElement});
