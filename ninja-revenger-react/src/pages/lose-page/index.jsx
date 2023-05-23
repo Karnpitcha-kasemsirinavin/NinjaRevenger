37476b91816125cacf6ae9b02c485f3b7c702aa1
KarnpitchaKasem <101696209+KarnpitchaKasem@users.noreply.github.com>
2023-05-23 12:12:21 +0700

    update new setting

--- ninja-revenger-react/src/pages/lose-page/index.jsx
+++ ninja-revenger-react/src/pages/lose-page/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useContext } from 'react'
 import Button from "../../Components/Button";
 import styles from "./style.module.css";
 
 import lightImage from "../../images/background-nolight.png";
 import smallImage from "../../images/loser.png";
+import { SocketContext } from "../../Context/SocketThing";
 
 function Box() {
+  const { socket, room, player_1} = useContext(SocketContext);
+
   return (
     <div className={styles.bg}>
       <img src={lightImage} className={styles.img} />
@@ -15,7 +19,7 @@ function Box() {
         <img 
             className='stars-result'
             // src={require("../../images/star"+ room.players[player_1].score +".png")}
-            src={require("../../images/star"+ 1 +".png")}
+            src={require("../../images/star"+ ( room.players[player_1]).score +".png")}
             alt='star0'
           />
         {/* <Button classname='result-btn' name='Play Again' type='stranger'/> */}
--- ninja-revenger-react/src/pages/win-page/index.jsx
+++ ninja-revenger-react/src/pages/win-page/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useContext } from 'react'
 import Button from "../../Components/Button/index.jsx";
 import styles from "./style.module.css";
 
 import backgroundImage from "../../images/lose.jpg";
 import lightImage from "../../images/light.png";
 import smallImage from "../../images/winner.png";
+import { SocketContext } from "../../Context/SocketThing";
+
+
 
 function Box() {
+  const { socket, room, player_1} = useContext(SocketContext);
+
   return (
     <>
     <div className={styles.mainCanvas}>
@@ -18,7 +24,7 @@ function Box() {
         <img 
             className='stars-result'
             // src={require("../../images/star"+ room.players[player_1].score +".png")}
-            src={require("../../images/star"+ 1 +".png")}
+            src={require("../../images/star"+( room.players[player_1]).score +".png")}
             alt='star0'
           />
         {/* <Button classname='result-btn' name='Play Again' type='stranger'/> */}
